1832abd37cec115b0793e99f3e6221ca64c0d9f5
arman <mdarmanhossen404@gmail.com>
2025-10-02 23:03:04 +0600

    todo list

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,6 @@ import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoComponent } from './Mycomponent/todos/todos';
 
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-
-
 
 @Component({
   selector: 'app-root',
