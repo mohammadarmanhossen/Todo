40e4e4f8bc44eb3765715162a90fe367af72a30a
arman <mdarmanhossen404@gmail.com>
2025-10-03 09:48:09 +0600

    update code

--- src/app/services/todo.service.spec.ts
+++ src/app/services/todo.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
   let service: TodoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        TodoService,
+        provideHttpClient(),        // ✅ HttpClient প্রোভাইড করা
+        provideHttpClientTesting()  // ✅ HttpClient testing প্রোভাইড করা
+      ],
+    });
+
     service = TestBed.inject(TodoService);
   });
 
